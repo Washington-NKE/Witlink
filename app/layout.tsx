60e367a565a15d0fce054e569bd9acc7960ec08d
Washington <washiem2022@gmail.com>
2025-06-01 23:45:46 +0300

    initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,35 @@
+import type { Metadata } from "next";
+import { Bricolage_Grotesque } from "next/font/google";
+import { ClerkProvider } from "@clerk/nextjs";
+import "./globals.css";
+import Navbar from "@/components/Navbar";
+
+const bricolage = Bricolage_Grotesque({
+  variable: "--font-bricolage",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Witlink",
+  description: "Real-time AI Teaching Platform",
+  appleWebApp: {
+    title: "WitLink"
+  }
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${bricolage.variable} antialiased`}>
+        <ClerkProvider appearance={{ variables: { colorPrimary: '#fe5933' }} }>
+          <Navbar />
+          {children}
+        </ClerkProvider>
+      </body>
+    </html>
+  );
+}
